99bde33d5ad4cfc710584d3851dc86c6ceba1472
Jitrotte <jitrotte@gmail.com>
2022-04-20 16:52:16 -0500

    Removed previous code

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-/*let myName = "Bob";
-document.write(myName);
-myName = "John Doe";
-document.write(myName);
-
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = doubleSize * 2 - size / 2;
-document.write(minSize);
-*/
-
-alert("Hello world");
-
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "pikachu", height: 5, type: ["electric", "ground"] },
+  { name: "bulbasaur", height: 6, type: ["grass", "poison"] },
+  { name: "squirtle", height: 7, type: ["water", "ground"] },
+];
