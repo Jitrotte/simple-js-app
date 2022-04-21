f0409e1ce98425dc1c28c16588b521c47856cec4
Jitrotte <jitrotte@gmail.com>
2022-04-20 22:11:52 -0500

    created for loop for pokemon array

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # simple-js-app
+
+This is a pokedex app that contains different pokemon. It lists their names, heights, and types.
+
+It uses a for loop to pull key values from an array along with a conditional for the tallest pokemon.
--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 let pokemonList = [
-  { name: "pikachu", height: 5, type: ["electric", "ground"] },
-  { name: "bulbasaur", height: 6, type: ["grass", "poison"] },
-  { name: "squirtle", height: 7, type: ["water", "ground"] },
+  {
+    name: "pikachu",
+    height: 5,
+    type: ["electric", "ground"],
+  },
+  {
+    name: "bulbasaur",
+    height: 6,
+    type: ["grass", "poison"],
+  },
+  {
+    name: "squirtle",
+    height: 7,
+    type: ["water", "ground"],
+  },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 6) {
+    document.write(
+      pokemonList[i].name +
+        " (height: " +
+        pokemonList[i].height +
+        ") WOW thats BIG!"
+    );
+  } else {
+    document.write(
+      pokemonList[i].name + " (height: " + pokemonList[i].height + ") <br/>"
+    );
+  }
+}
